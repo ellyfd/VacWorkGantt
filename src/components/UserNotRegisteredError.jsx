dc6a11d86f29b0fb1682ab97a3b8afa553069d55
Elly Cheng <ellyfd@gmail.com>
2026-02-23 14:25:48 +0000

    File changes

---
+++ src/components/UserNotRegisteredError.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// Placeholder component - this was previously deleted but App.jsx still references it
+export default function UserNotRegisteredError() {
+  return null;
+}
