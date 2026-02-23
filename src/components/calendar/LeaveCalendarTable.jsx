b2595cae8b300495a63d26d0f241761061e566fd
Elly Cheng <ellyfd@gmail.com>
2026-02-23 13:45:05 +0000

    File changes

--- src/components/calendar/LeaveCalendarTable.jsx
+++ src/components/calendar/LeaveCalendarTable.jsx
@@ -182,7 +182,7 @@ export default function LeaveCalendarTable({
                   })}
                 </tr>
               );
-          )}
+            })}
         </tbody>
         </table>
         </div>
