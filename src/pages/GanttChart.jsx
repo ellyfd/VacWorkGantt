11288ad6777bd2c2deb364b17b5090b44f5c5483
Elly Cheng <ellyfd@gmail.com>
2026-02-24 13:43:39 +0000

    File changes

--- src/pages/GanttChart.jsx
+++ src/pages/GanttChart.jsx
@@ -256,7 +256,7 @@ export default function GanttChart() {
       queryClient.invalidateQueries(['ganttTasks']);
       setShowAddTaskDialog(false);
       setTaskFormData({ name: '', is_important: false, note: '', time_type: '', start_date: '', end_date: '' });
-      setCreatingProjectId(null);
+      setCreatingProjectIdSync(null);
     },
   });
 
